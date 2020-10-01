773219bf5e8957191700f94dded91f54134b71da
Nadine Ebrani <nadinedoshkeh3@gmail.com>
2020-10-01 17:01:18 -0400

    Updates

--- app.js
+++ app.js
@@ -2,17 +2,36 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
+const { db, Page, User } = require('./models');
+
+
 app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(__dirname +'/public'));
 
 
 app.get('/', (req,res,next)=>{
-    res.send('');
+    res.send('hello world');
 });
+app.get('/page', async (req, res, next) => {
+    await console.log(Page)
+    res.send(Page)
+})
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
+  const PORT = 1337;
+//db.sync({force:true})
+const init = async () => {
+   
+    await db.sync({force:true})
+    app.listen(PORT, ()=>{
+        console.log(`App listening in PORT ${PORT}`)
+        });
+}
+init()
 
 
-const PORT = 1337;
-app.listen(PORT, ()=>{
-console.log(`App listening in PORT ${PORT}`)
-});
+
+
