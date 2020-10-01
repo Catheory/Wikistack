66d6fcd06145f37e757399455d50a5d138934495
Sirui Sun <catheory91@gmail.com>
2020-10-01 12:43:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({extended:false}));
+
+// app.use(express.static(__dirname, 'public'));
+
+
+app.get('/', (req,res,next)=>{
+    res.send('hello world');
+});
+
+
+const PORT = 1337;
+app.listen(PORT, ()=>{
+console.log(`App listening in PORT ${PORT}`)
+});
